4ca5924e0c7059adc62e56773ed5531f216ce78f
Sofiadoingcode <88271469+Sofiadoingcode@users.noreply.github.com>
2023-01-24 11:40:09 +0100

    Maybe Fixes

--- src/main/java/facades/Populator.java
+++ src/main/java/facades/Populator.java
@@ -5,8 +5,15 @@
  */
 package facades;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+
+import entities.Role;
+import entities.User;
 import utils.EMF_Creator;
+
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  *
@@ -15,6 +22,30 @@ import utils.EMF_Creator;
 public class Populator {
     public static void populate(){
         EntityManagerFactory emf = EMF_Creator.createEntityManagerFactory();
+        EntityManager em = emf.createEntityManager();
+
+        em.getTransaction().begin();
+
+        Role user = new Role("user");
+        Role admin = new Role("admin");
+        User user1 = new User("useradmin", "ua123");
+        User user2 = new User("admin", "a123");
+        User user3 = new User("user", "u123");
+
+        user1.addRole(user);
+        user1.addRole(admin);
+        user2.addRole(admin);
+        user3.addRole(user);
+
+        em.persist(user);
+        em.persist(admin);
+        em.persist(user1);
+        em.persist(user2);
+        em.persist(user3);
+
+
+        em.getTransaction().commit();
+        em.close();
 
     }
     
